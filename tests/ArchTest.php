5b382201e71ccfc0f584665558b64504a8b6d5be
Emmanuel Okeke <emmanix2002@gmail.com>
2026-04-15 12:10:13 +0100

    feat: Support for Laravel 13

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-beforeEach()->coversNothing();
+beforeEach()->covers();
 
 arch('it does not call dd()')->expect('dd')->not->toBeUsed();
 arch('it does not call ddd()')->expect('ddd')->not->toBeUsed();
@@ -10,7 +10,7 @@ arch('it does not call dump()')->expect('dump')->not->toBeUsed();
 arch('it does not call xdebug_break()')->expect('xdebug_break')->not->toBeUsed();
 
 arch('it meets PHP preset')
-    ->skip(fn () => !version_compare(Pest\version(), '3.0.0', '>='), 'Requires Pest 3+')
+    ->skip(fn () => ! version_compare(Pest\version(), '3.0.0', '>='), 'Requires Pest 3+')
     ->preset()
     ->php()
     ->ignoring(['var_export', 'debug_backtrace']);
